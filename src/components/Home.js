42aafc0ef6783fa65af47e32bc669d22ff7ce802
Rasec <rasec@Rasecs-MacBook-Air.local>
2021-08-05 13:01:07 -0500

    Add Grid component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,11 @@ import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
 
 // Components
 import HeroImage from './HeroImage'
+import Grid from './Grid'
 
 // Hooks
 import {useHomeFetch} from '../hooks/useHomeFetch'
+
 // Images
 import NoImage from '../images/no_image.jpg';
 
@@ -24,6 +26,12 @@ const Home = () =>{
                 title = {state.results[0].original_title}
                 text = {state.results[0].overview}/>
         ) : null}
+
+        <Grid header = 'Popular Movies' >
+            {state.results.map(movie => (
+                <div key={movie.id}>{movie.title}</div>
+            ))}
+        </Grid>
         </>
     );
 }
