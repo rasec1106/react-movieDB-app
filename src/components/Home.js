a5cbb1c775d33f79f1b1be44346d7bf9f2740d1c
Rasec <rasec@Rasecs-MacBook-Air.local>
2021-08-05 11:38:23 -0500

    Change home page visualitazion. Add HeroImage

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,7 @@ import React, {useState, useEffect} from 'react';
 import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
 
 // Components
+import HeroImage from './HeroImage'
 
 // Hooks
 import {useHomeFetch} from '../hooks/useHomeFetch'
@@ -14,7 +15,17 @@ const Home = () =>{
     
     const {state, loading, error} = useHomeFetch();
     console.log(state);
-    return <div>HomePage</div>
+    return (
+        // (<> </>) => fragment to encapsulate all that we are returning 
+        <> 
+        { state.results[0] ? (
+                <HeroImage 
+                image = {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title = {state.results[0].original_title}
+                text = {state.results[0].overview}/>
+        ) : null}
+        </>
+    );
 }
 
 export default Home;
