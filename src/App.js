519890cc2a63563f8e7192ff59c1c11013d6c946
CesarHerrera <elch.herrera@gmail.com>
2021-08-06 11:58:14 -0500

    Add Routing logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 
+// Routing
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
 //Components
-import Header from './components/Header'
-import Home from './components/Home'
+import Header from './components/Header';
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 
 //Styles
 import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header/>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        {/* '/:movieId' is a parameter for the Route component */}
+        <Route path='/:movieId' element={<Movie/>}/> 
+        {/* '/*' is for any other path */}
+        <Route path='/*' element={<NotFound/>}/> 
+      </Routes>
       <GlobalStyle />
-    </div>
+    </Router>
   );
 }
 
