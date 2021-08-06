23af94962803748a70c967ba129a96d47a41fbc0
CesarHerrera <elch.herrera@gmail.com>
2021-08-06 18:26:37 -0500

    Reallocate Movie component

---
+++ src/components/Movie.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+// Config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+
+// Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+
+// Hook
+
+// Image
+import NotFound from './NotFound';
+
+const Movie = () => {
+    return (
+    <div>Movie</div>
+    )
+};
+
+export default Movie;
