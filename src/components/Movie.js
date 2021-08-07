aabdf5fc1de04dadfda17d3f24707afd2bd73fa9
CesarHerrera <elch.herrera@gmail.com>
2021-08-07 16:37:42 -0500

    Add logic and BreadCrumb component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,31 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 // Config
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
 // Components
+import BreadCrumb from './BreadCrumb';
 import Grid from './Grid';
 import Spinner from './Spinner';
 
 // Hook
+import { useMovieFetch } from '../hooks/useMovieFetch';
 
 // Image
 import NotFound from './NotFound';
 
 const Movie = () => {
+
+    const {movieId} = useParams();
+    const {state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log(movie);
+    if(loading) return <Spinner/>;
+    if(error) return <div>Something went wrong...</div>;
+
     return (
-    <div>Movie</div>
+        <BreadCrumb movieTitle = {movie.original_title}/>
     )
 };
 
