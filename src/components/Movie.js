bef8bd17f25dd56de703bee2d328fac84ed91291
CesarHerrera <elch.herrera@gmail.com>
2021-08-07 18:22:15 -0500

    Add MovieInfo component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,7 @@ import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
 
 // Components
 import BreadCrumb from './BreadCrumb';
+import MovieInfo from './MovieInfo';
 import Grid from './Grid';
 import Spinner from './Spinner';
 
@@ -25,8 +26,11 @@ const Movie = () => {
     if(error) return <div>Something went wrong...</div>;
 
     return (
-        <BreadCrumb movieTitle = {movie.original_title}/>
-    )
+        <>
+            <BreadCrumb movieTitle = {movie.original_title}/>
+            <MovieInfo movie = {movie} />
+        </>
+    );
 };
 
 export default Movie;
